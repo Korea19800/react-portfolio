ee9dd297640f02b24cb8f1149372ffe09c83ecaa
Korea19800 <korea19800@gmail.com>
2024-11-11 19:05:18 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/About';
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
