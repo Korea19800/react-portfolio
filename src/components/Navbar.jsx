28ddb5e41d65732b6c33db639479248aca509865
Korea19800 <korea19800@gmail.com>
2024-11-11 19:07:59 -0700

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import {FaLinkedin} from "react-icons/fa"
 import {FaGithub} from "react-icons/fa"
 import {FaInstagram} from "react-icons/fa"
 
-
 const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
